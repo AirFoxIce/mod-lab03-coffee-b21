c9169b296c5904204e6ae1f40a9cb2fda79bf1a5
Ivan Vladimirov <vivalexy@ya.ru>
2025-04-06 18:42:36 +0300

    Добавлены тесты

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,84 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(AutomataTest, StartsInOffState) {
+    Automata a;
+    EXPECT_EQ(a.getState(), OFF);
+}
+
+TEST(AutomataTest, TurnsOnToWaitState) {
+    Automata a;
+    a.on();
+    EXPECT_EQ(a.getState(), WAIT);
+}
+
+TEST(AutomataTest, CoinChangesStateToAccept) {
+    Automata a;
+    a.on();
+    a.coin(20);
+    EXPECT_EQ(a.getState(), ACCEPT);
+}
+
+TEST(AutomataTest, CoinAccumulatesCash) {
+    Automata a;
+    a.on();
+    a.coin(10);
+    a.coin(20);
+    a.choice(0);
+    EXPECT_TRUE(a.check(0));
+}
+
+TEST(AutomataTest, ValidChoiceGoesToCheck) {
+    Automata a;
+    a.on();
+    a.coin(30);
+    a.choice(1); // Coffee
+    EXPECT_EQ(a.getState(), CHECK);
+}
+
+TEST(AutomataTest, CheckFailsIfNotEnoughCash) {
+    Automata a;
+    a.on();
+    a.coin(5);
+    a.choice(2);
+    EXPECT_FALSE(a.check(2));
+    EXPECT_EQ(a.getState(), ACCEPT);
+}
+
+TEST(AutomataTest, CookFromCheckState) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(1);
+    EXPECT_TRUE(a.check(1));
+    a.cook(1);
+    EXPECT_EQ(a.getState(), COOK);
+}
+
+TEST(AutomataTest, FinishReturnsToWait) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.choice(1);
+    a.check(1);
+    a.cook(1);
+    a.finish(1);
+    EXPECT_EQ(a.getState(), WAIT);
+}
+
+TEST(AutomataTest, CancelResetsStateAndCash) {
+    Automata a;
+    a.on();
+    a.coin(30);
+    a.cancel();
+    EXPECT_EQ(a.getState(), WAIT);
+}
+
+TEST(AutomataTest, CannotOffFromAccept) {
+    Automata a;
+    a.on();
+    a.coin(30);
+    a.off(); // не должен сработать
+    EXPECT_NE(a.getState(), OFF);
+}
